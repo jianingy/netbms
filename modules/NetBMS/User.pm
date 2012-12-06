d109d8705a79c99712d617e600622f01335a041e
Jianing YANG <jianingy.yang@gmail.com>
2012-12-06 08:59:02 +0800

    splitted configuration out

--- modules/NetBMS/User.pm
+++ modules/NetBMS/User.pm
@@ -4,22 +4,50 @@
 # created at : 2012-12-04 19:06:51
 # author     : Jianing Yang <jianingy.yang AT gmail DOT com>
 
+
+# Account Server could be any server honor the following JSON-RPC spec
+# 
+# 1. required function: acct(node=[nodename])
+# 2. required return: acctname, acctuid, acctpass
+#
+# Here, acctpass is an encrypted password string
+#
+
+# Configuration
+# create a file named config.pl in the same directory where netbms.pl 
+# placed with the following content
+#
+# $account_server = 'http://[accountserver]'
+# $account_default_gid = [default group id]
+
 package NetBMS::User;
+
+use strict;
+use warnings;
 
 use JSON::RPC::Client;
 use Data::Dumper;
 use Sys::Hostname;
+use FindBin qw/$Bin/;
 
-my $account_server = 'http://192.168.1.111:80/postgres/';
+our $account_server = undef; 
+our $account_default_gid = undef;
+
+require "$Bin/conf/config.pl";
 
 sub sync
 {
+
       my %users; 
       my $client = new JSON::RPC::Client;
+
+      &main::err('user module configuration error')
+          unless defined($account_server) && defined($account_default_gid);
 
       $client->prepare($account_server, ['acct']);
       foreach (@{$client->acct({_node => hostname})}) {
 	  my %user = %{$_};
+          #print STDERR Dumper(\%user);
 	  my @entry = getpwnam($user{acctname});
 
           $users{$user{acctname}} = 1; # log canonical user;
@@ -29,12 +57,12 @@ sub sync
               my $uid = $entry[2];
               &main::out('WARN', "uid doesn't match ($uid <-- $user{acctuid})")
                   if $uid != $user{acctuid};
-              my $out = qx#/usr/sbin/usermod 2>&1 -u '$user{acctuid}' -g 500 -p '$user{acctpass}' '$user{acctname}'#;
+              my $out = qx#/usr/sbin/usermod 2>&1 -u '$user{acctuid}' -g $account_default_gid -p '$user{acctpass}' '$user{acctname}'#;
               &main::out('ERROR', "usermod failed: $out") if $?;
 	  } else {
               # add new user 
               &main::out('INFO', "newuser $user{acctname}");
-              my $out = qx#/usr/sbin/useradd 2>&1 -u '$user{acctuid}' -g 500 -p '$user{acctpass}' '$user{acctname}'#;
+              my $out = qx#/usr/sbin/useradd 2>&1 -u '$user{acctuid}' -g $account_default_gid -p '$user{acctpass}' '$user{acctname}'#;
               &mainLLout('ERROR', "useradd failed: $out") if $?;
 	  }
       }
@@ -53,3 +81,6 @@ sub sync
 
 
 1;
+
+
+#vim: ts=4 sw=4 ai et
